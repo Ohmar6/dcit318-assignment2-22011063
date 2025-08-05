37ccf05e180c576cf97d20bbda04d7a93a7a199e
Mangortey Gideon Adjartey <gideonadjartey5@gmail.com>
2025-08-05 23:13:52 +0000

    Task 1 - Inheritance: Animal, Dog, Cat classes

---
+++ Task1-Inheritance/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace InheritanceAndOverriding
+{
+    // Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class: Dog
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class: Cat
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of each class
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            // Call MakeSound() on each instance
+            Console.WriteLine("Animal:");
+            genericAnimal.MakeSound();
+
+            Console.WriteLine("\nDog:");
+            dog.MakeSound();
+
+            Console.WriteLine("\nCat:");
+            cat.MakeSound();
+        }
+    }
+}
