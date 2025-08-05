9f7df7dfcc194821e41c8994d7c2a0e9ed53337f
Mangortey Gideon Adjartey <gideonadjartey5@gmail.com>
2025-08-05 23:23:34 +0000

    Task 3 - Interface: IMovable with Car and Bicycle

---
+++ Task3-Interface/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    // Define the interface
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Class that implements IMovable
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Another class that implements IMovable
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Car and Bicycle
+            IMovable myCar = new Car();
+            IMovable myBike = new Bicycle();
+
+            // Call Move() on each
+            myCar.Move();
+            myBike.Move();
+        }
+    }
+}
